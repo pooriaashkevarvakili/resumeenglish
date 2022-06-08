a6efadb3f5c1b93a218d981fd59cb9e21e9ad8f0
pooriavakili <pooriavakili09@gmail.com>
2022-06-08 14:23:15 +0430

    updatesocialnetwork

--- src/pages/SocialNetwork.vue
+++ src/pages/SocialNetwork.vue
@@ -1,34 +1,65 @@
 <template>
   <div class="q-pa-md">
     <div class="row">
-      <div class="col-md-3 col-lg-3 col-xs-12 col-sm-12">
+      <div class="col-md-4 col-lg-4 col-xs-12 col-sm-12 text-center">
         <a href="https://github.com/pooriaashkevarvakili">
           <q-card>
-            <q-card-section class="">
+            <q-card-section class="text-center">
               <img class="q-ml-sm" src="github.png" style="" alt="">
             </q-card-section>
           </q-card>
         </a>
       </div>
-      <div class="col-md-3 col-lg-3 col-xs-12 col-sm-12">
+      <div class="col-md-4 col-lg-4 col-xs-12 col-sm-12 text-center">
         <a href="https://telegram.me/pooriaashkevarvakili">
           <q-card>
-            <q-card-section class="">
+            <q-card-section class="text-center">
               <img class="q-ml-sm" src="telegram.jpg" style="" alt="">
             </q-card-section>
           </q-card>
         </a>
       </div>
-      <div class="col-md-3 col-lg-3 col-xs-12 col-sm-12">
+      <div class="col-md-4 col-lg-4 col-xs-12 col-sm-12 text-center">
         <a href="https://www.linkedin.com/in/pooria-ashkevar-vakili-6457a917a/">
           <q-card>
-            <q-card-section class="">
+            <q-card-section class="text-center">
               <img class="q-ml-sm" src="linkedin.png" style="" alt="">
             </q-card-section>
           </q-card>
         </a>
       </div>
 
+
+
+    </div>
+    <div class="row " style="margin-top:20px">
+      <div class="col-md-4 col-lg-4 col-xs-12 col-sm-12 text-center ">
+        <a href="https://gitlab.com/pooriaashkevarvakili">
+          <q-card>
+            <q-card-section class="">
+              <img class="q-ml-sm" src="gitlab.png" style="" alt="">
+            </q-card-section>
+          </q-card>
+        </a>
+      </div>
+      <div class="col-md-4 col-lg-4 col-xs-12 col-sm-12 text-center">
+        <a href="https://stackoverflow.com/users/13022538/pooriaashkevarvakili">
+          <q-card>
+            <q-card-section class="">
+              <img class="q-ml-sm" src="stackowerflow.png" style="" alt="">
+            </q-card-section>
+          </q-card>
+        </a>
+      </div>
+      <div class="col-md-4 col-lg-4 col-xs-12 col-sm-12 text-center">
+        <a href="https://join.skype.com/invite/ftQWTpaxGlKg">
+          <q-card>
+            <q-card-section class="">
+              <img class="q-ml-sm" src="skype.png" style="" alt="">
+            </q-card-section>
+          </q-card>
+        </a>
+      </div>
     </div>
   </div>
 </template>
